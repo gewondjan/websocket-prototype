f141b869e43a5e77508721f0b08b52929494a0d6
Ryan Gewondjan <ryan.gewondjan@gmail.com>
2019-04-02 17:48:38 -0600

    added the port to listen on

--- server.js
+++ server.js
@@ -6,5 +6,7 @@ var app = express();
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', webSocketTest.html));
+    res.sendFile(path.join(__dirname, 'public', 'webSocketTest.html'));
 });
+
+app.listen(3000);
